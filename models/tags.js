c97a197122e142bb7dedbbc345093aea678c7cc3
S. Everett Abbott <severettabbott@gmail.com>
2018-03-22 15:32:51 -0700

    Reshaped the mock data to work with seeds

---
+++ models/tags.js
@@ -0,0 +1,20 @@
+const shoeData = require('../resources/shoeData.json');
+
+let tags = [];
+
+shoeData.forEach(shoe => {
+    shoe.tags.forEach(tag => {
+        if (!tags.includes(tag)) {
+            tags.push(tag);
+        }
+    });
+});
+
+tags = tags.map((tag, i) => {
+    return {
+        id: i+1,
+        name: tag
+    };
+});
+
+module.exports = { tags };
