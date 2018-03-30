f2a9afb7a6ea36809bc9a696afcb41ee7134cd17
Jordan Deam <jordan.deam@gmail.com>
2018-03-30 10:10:12 -0700

    Removed console log

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const controller = require('../controllers');
 const router = express.Router();
-console.log(controller.reviews);
 router.post('/', controller.reviews.createReview);
 
 
